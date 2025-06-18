cc02c65ec760975bb49f357d40f562200724bdf9
Ivan Mendez <ivanmw97@gmail.com>
2025-06-18 15:09:14 +0100

    Initial commit for ClipNest

---
+++ ClipNest/ClipNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipNestApp.swift
+//  ClipNest
+//
+//  Created by Ivan Mendez Westlund on 18/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipNestTests/ClipNestTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipNestTests.swift
+//  ClipNestTests
+//
+//  Created by Ivan Mendez Westlund on 18/6/25.
+//
+
+import Testing
+@testable import ClipNest
+
+struct ClipNestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
